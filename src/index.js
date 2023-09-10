b8d5ff12d66d2da1b600cd02ba0c04a9d1ec937e
Exter555 <dandyanugerah004@gmail.com>
2023-09-10 20:18:41 +0700

    y

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 require('./vendor');
 import css from './style.scss';
+import _ from 'lodash';
 import { run } from './app/app.js';
 import { AlertService } from './app/alert.service.js';
 import { CalculatorService } from './app/calculator.service.js';
